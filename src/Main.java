1807422117a9c8398b8f1fa969335170f654aceb
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-19 23:23:10 +0300

    taskEnd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        String firstName = "Ivan";
+        String firstName = "Ivan ";
         String middleName = "Ivanovich";
         String lastName = "Ivanov ";
-        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName = lastName + firstName + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
        String fullName2 = fullName.toUpperCase();
     System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
